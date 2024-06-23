bb5d8540764bba9a2cb1d0e49474c81eda019721
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2024-06-23 12:23:27 +0300

    react excercises continue

--- part1/react-excercises/src/App.jsx
+++ part1/react-excercises/src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
-
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
+
   console.log('rendering...', counter)
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus' />
     </div>
   )
 }
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 export default App
